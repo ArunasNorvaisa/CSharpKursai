f0c7d862af3ccbbb921e0e047311e1ae15f2525e
Arunas Norvaisa <arunas.norvaisa@bdo.global>
2019-12-13 07:51:13 +0200

    pamoka3 sukurtas

---
+++ Pamoka3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpKursai
+{
+	public class Pamoka3
+	{
+
+// test if number is primal
+
+		bool testPrimal(int nr)
+		{
+			for(int i = 2; i < Math.Sqrt(nr); i++)
+			{
+				if (nr % i == 0) return false;
+			}
+			return true;
+		}
+	}
+}
