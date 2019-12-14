dc8533c4f6696bffec59095224fe92e0c38f7695
Arūnas Norvaiša <an@arunas.org>
2019-12-14 12:52:56 +0200

    Lesson 3 main part solved.

--- Program.cs
+++ Program.cs
@@ -4,11 +4,24 @@ namespace CSharpKursai
   {
     private static void Main()
     {
-      Pamoka1.ApskritimoIlgisPlotas(1.2);
-      Pamoka1.GreitisKmh(3001, 151);
-      Pamoka1.MokinioIvertinimas(10);
-      Pamoka1.MokinioIvertinimas(-15);
-      Pamoka1.DidziausiasSkaicius(-1, -15.15, 23.054);
+      // Pamoka1:
+      // Pamoka1.ApskritimoIlgisPlotas(1.2);
+      // Pamoka1.GreitisKmh(3001, 151);
+      // Pamoka1.MokinioIvertinimas(10);
+      // Pamoka1.MokinioIvertinimas(-15);
+      // Pamoka1.DidziausiasSkaicius(-1, -15.15, 23.054);
+      
+      //Pamoka3:
+      Pamoka3.Meniu();
+      Pamoka3.KasTreciasSkaicius(34);
+      Pamoka3.Kvadratai();
+      Pamoka3.DaliniuSuma(3434);
+      Pamoka3.Fibonacci(23);
+      Pamoka3.SumaIki1000();
+      Pamoka3.SumaIki1000Papildymas1();
+      Pamoka3.SumaIki1000Papildymas2();
+      Pamoka3.SumaIki1000Pakeista();
+      Pamoka3.AtsitiktiniaiSkaiciai.AtspekSkaiciu();
     }
   }
 }
