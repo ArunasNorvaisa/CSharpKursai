1e5747c86b4789b17cc8efde4eab6c5226458b72
Arunas Norvaisa <arunas.norvaisa@bdo.global>
2020-01-16 12:20:41 +0200

    RectangleDraw created

---
+++ ToDoList/ConsoleTodoListApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleTodoListApp.GUI;
+using System;
+
+namespace ConsoleTodoListApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Rectangle rectangle = new Rectangle(5, 5, 15, 10, '#');
+
+      rectangle.Draw();
+    }
+  }
+}
